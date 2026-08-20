11c3a678f7a748ffb4770139743922be2ec63e8c
artin231 <leiartrezjah@gmail.com>
2026-08-20 18:51:47 +0330

    new styles

--- HooshangAlert.js
+++ HooshangAlert.js
@@ -4,27 +4,30 @@ class Hooshang {
     }
     AlHoosh(color, title, message, icon, type, buttonMessage = 'click me') {
         let NewElement = `
-        <div class='ggbhlkfdiughwroihgowehgowehgoihewioghpewhgohwoehgoehgoh hghghghghg${color}' >
-        <h1 class='kiqfngioqehioghqeoihgoehogjeoighoeh'>${title}</h1>
-        <p class='jrwiognrowingorngorwnognorngworogbrogorn'>${message}</p>
-        ${type == 'button' && `<button class="avadiqegoeogeoqgboeb">${buttonMessage}</button>`}
+        <div class='hfuewiooihegeighowhggigwrohgowrhg'>
+                <div class='ggbhlkfdiughwroihgowehgowehgoihewioghpewhgohwoehgoehgoh hghghghghg${color}' >
+                    <h1 class='kiqfngioqehioghqeoihgoehogjeoighoeh'>${title}</h1>
+                    <p class='jrwiognrowingorngorwnognorngworogbrogorn'>${message}</p>
+                    ${type == 'button' && `<button class="avadiqegoeogeoqgboeb">${buttonMessage}</button>`}
+            </div>
         </div>
+
         `
 
         document.body.insertAdjacentHTML('afterbegin', NewElement)
         try {
             let btn = document.querySelector('.avadiqegoeogeoqgboeb');
-            if (btn){
-                btn.addEventListener('click',() => {
-                    let removeEl = document.querySelector('.ggbhlkfdiughwroihgowehgowehgoihewioghpewhgohwoehgoehgoh');
+            if (btn) {
+                btn.addEventListener('click', () => {
+                    let removeEl = document.querySelector('.hfuewiooihegeighowhggigwrohgowrhg');
                     removeEl.remove();
                 })
             }
-        }catch{
-            
+        } catch {
+
         }
-      
-        
+
+
     }
 }
 let hooshang = new Hooshang();
--- Test.js
+++ Test.js
@@ -1,3 +1,5 @@
 import hooshang from "./HooshangAlert.js";
 
+setTimeout(() => {
 hooshang.AlHoosh('red','title','message','ali','button','aaaa');
+},2000)
