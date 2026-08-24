db4172b9889be915751614308f69285b552adf80
artin231 <leiartrezjah@gmail.com>
2026-08-24 18:19:51 +0330

    adding the icons object

--- HooshangAlert.js
+++ HooshangAlert.js
@@ -1,15 +1,15 @@
 class Hooshang {
     constructor() {
-
+	this.Icons = {succses:"<i class='fa-regular fa-square-check' id='grwhrwhwhhrfhrehkrejhprejhprjhpj'></i>"};
     }
     AlHoosh(color, title, message, icon, type, buttonMessage = 'click me') {
         let NewElement = `
         <div class='hfuewiooihegeighowhggigwrohgowrhg'>
                 <div class='ggbhlkfdiughwroihgowehgowehgoihewioghpewhgohwoehgoehgoh hghghghghg${color}' >
-                <i class="fa-regular fa-square-check" id='grwhrwhwhhrfhrehkrejhprejhprjhpj'></i>
-                    <h1 class='kiqfngioqehioghqeoihgoehogjeoighoeh'>${title}</h1>
-                    <p class='jrwiognrowingorngorwnognorngworogbrogorn'>${message}</p>
-                    ${type == 'button' && `<button class="avadiqegoeogeoqgboeb">${buttonMessage}</button>`}
+			${icon}
+                    	<h1 class='kiqfngioqehioghqeoihgoehogjeoighoeh'>${title}</h1>
+                    	<p class='jrwiognrowingorngorwnognorngworogbrogorn'>${message}</p>
+                    	${type == 'button' && `<button class="avadiqegoeogeoqgboeb">${buttonMessage}</button>`}
             </div>
         </div>
 
--- Test.js
+++ Test.js
@@ -1,5 +1,5 @@
 import hooshang from "./HooshangAlert.js";
 
 setTimeout(() => {
-hooshang.AlHoosh('red','title','fiweogowhnbnnnnnnnnnnnnnnnnretugnrwnhgowrighoewriognpewngi','ali','button','aaaa');
+hooshang.AlHoosh('red','عملیات با موفقیت انجام شد ','عملیات با مفقیت انجام شد و به مشکلی بر نخورد',hooshang.Icons.succses,'button','تایید');
 },2000)
