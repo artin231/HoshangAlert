29fa2b83d7a3c3e6175c1505637f90b9d31f8292
artin231 <leiartrezjah@gmail.com>
2026-09-01 09:18:04 +0330

    making the alert responsivew

--- scriptTest.js
+++ scriptTest.js
@@ -1,3 +1,3 @@
 import hooshang from './HooshangAlert.js';
 
-hooshang.AlHoosh({theme:"light",title:'succses',message:"it was succsesful",icon:hooshang.Icons.succses,type:"time",buttonMessage:"click me",color_icon:'green',time:5000})
+hooshang.AlHoosh({theme:"light",title:'succseswqrwqrwr',message:"it was succsesful are ererererererereerer",icon:hooshang.Icons.succses,type:"button",buttonMessage:"click me",color_icon:'green',time:5000})
